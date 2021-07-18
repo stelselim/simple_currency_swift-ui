11483c12716934d272fcb85e84a38053b313563c
stelselim <selim.ustel1998@gmail.com>
2021-07-18 18:22:31 +0300

    Initial Commit

---
+++ simple_currency/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  simple_currency
+//
+//  Created by Selim Üstel on 18.07.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ simple_currency/simple_currencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  simple_currencyApp.swift
+//  simple_currency
+//
+//  Created by Selim Üstel on 18.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct simple_currencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
